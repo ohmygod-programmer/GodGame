cdf01ace85b3d28656ebe4446d1cf81598d79e03
ohmygod-programmer <dimantaras9@yandex.ru>
2023-10-13 18:29:10 +0700

    Init

--- GodGameTests/DeckTests.cs
+++ GodGameTests/DeckTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using GodGameLibrary;
 namespace GodGameTests
 {
@@ -6,7 +7,7 @@ namespace GodGameTests
     public class DeckTests
     {
         [TestMethod]
-        public void ColorsTest()
+        public void ColorsInStandardDeckTest()
         {
             Deck deck = Deck.GetStandardDeck();
             int size = deck.Size();
@@ -26,9 +27,15 @@ namespace GodGameTests
             }
             Assert.AreEqual(18, red_c);
             Assert.AreEqual(18, black_c);
+        }
+        [TestMethod]
+        public void ColorsInShuffledDeckTest()
+        {
+            Deck deck = Deck.GetStandardDeck();
+            int size = deck.Size();
             deck.RandomShuffle();
-            red_c = 0;
-            black_c = 0;
+            int red_c = 0;
+            int black_c = 0;
             for (int i = 0; i < size; i++)
             {
                 if (deck.GetCard(i).Color == Colors.black)
@@ -42,6 +49,39 @@ namespace GodGameTests
             }
             Assert.AreEqual(18, red_c);
             Assert.AreEqual(18, black_c);
+        }
+        [TestMethod]
+        public void ColorsInSlicedDeckTest()
+        {
+            Deck deck = Deck.GetStandardDeck();
+            int size = deck.Size();
+            deck.RandomShuffle();
+            Deck deck1 = deck.GetFirstNCardsDeck(size / 2);
+            Deck deck2 = deck.GetLastNCardsDeck(size / 2);
+            int red_c = 0;
+            int black_c = 0;
+            for (int i = 0; i < size/2; i++)
+            {
+                if (deck1.GetCard(i).Color == Colors.black)
+                {
+                    black_c++;
+                }
+                else if (deck1.GetCard(i).Color == Colors.red)
+                {
+                    red_c++;
+                }
+                if (deck2.GetCard(i).Color == Colors.black)
+                {
+                    black_c++;
+                }
+                else if (deck2.GetCard(i).Color == Colors.red)
+                {
+                    red_c++;
+                }
+            }
+            Assert.AreEqual(18, red_c);
+            Assert.AreEqual(18, black_c);
+
         }
     }
 }
